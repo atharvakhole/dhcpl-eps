ca19afb4d6bcff1851a1fb848a592f071f13b179
Atharva Khole <atharvakhole26@gmail.com>
2025-09-27 15:35:33 +0800

    Setup file logging

--- plant_control/app/runtime/service_runtime.py
+++ plant_control/app/runtime/service_runtime.py
@@ -1,16 +1,17 @@
 from plant_control.app.core.tag_service import TagService
 from plant_control.app.config import config_manager
 from plant_control.app.core.connection_manager import connection_manager
+from plant_control.app.utilities.logging_examples import setup_file_logging
 from plant_control.app.utilities.telemetry import logger
 
 
 class ServiceRuntime:
-    def __init__(self):
+    def __init__(self, log_file_path: str):
         self.config_manager = config_manager
         self.tag_service = None
         self.procedure_executor = None
         self.register_maps = None
-        self.logger = logger
+        self.logger = setup_file_logging(log_file_path)
 
 
     async def start(self):
